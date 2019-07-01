93c7e813fa8a55846c74fecc9707cfc6d7068a4a
Lisa Leu <Lisal@netwealth.com.au>
2019-07-01 18:19:45 +1000

    attempting to structure game code as an object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,26 +3,32 @@
 
  // The array of words for the game
 
- var wordLIst = ["bodyguard", "bojack horseman", "when they see us", "archer", "russian doll", "black mirror", "ozark"];
-
+ 
  // Pick a random word
- var word = wordList[Math.floor(Math.random() * words.length)];
- var wins = 0
- var guessCount = 0;
- var maxGuesses = 15;
+ var game = {
+    wordList: ["bodyguard", "bojack horseman", "when they see us", "archer", "russian doll", "black mirror", "ozark"];
+    word: wordList[Math.floor(Math.random() * words.length)],
+    winsCount: 0,
+    guessCount: 0,
+    maxGuesses: 15,
 
- // Letters for random word array = [];
- for (var i = 0; i < word.length; i++) {
-     wordList[i] = "_";
- }
+    startGame: function() {
+        document.getElementById("word").innerHTML = game.word;
 
+        listLetterSpaces() {
+        for (var i = 0; i < word.length; i++) {
+        answerArray[i] = "_";
+        };
+    }
+   
+var 
 
  // FUNCTIONS
  // ==============================================================================
 
- function captureData() {
-    console.log();
- }
+ //function captureData() {
+    //console.log();
+// }
 
  // MAIN PROCESS
  // ==============================================================================
@@ -32,6 +38,10 @@
  document.onkeyup = function (event) {
 
      //select random word
+    var game.word = event.key;
+    
+     //display spaces
+     document.getElementById('guessesRemaining').innerHTML =
 
      //display picture
 
@@ -43,11 +53,14 @@
 
      // Captures the key press, converts it to lowercase, and saves it to a variable.
      var letter = event.key.toLowerCase();
-     console.log(letter);
+
+     console.log(letter);}
 
      //if function - if letter is in the word, post to the letter space on the UI; else....
 
      //reduce guesses remaining and post to UI
+
+     document.getElementById('guessesRemaining').innerHTML = remainingLetters;
 
      //post letter guessed to the UI
      document.getElementById("lettersGuessed");
