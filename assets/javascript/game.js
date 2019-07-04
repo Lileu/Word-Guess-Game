9b15d762b864bc01a20fdd95175830270a4bd07c
Lisa Leu <Lisal@netwealth.com.au>
2019-07-04 14:46:36 +1000

    start game method added but still debugging textContent wordToGuess ID

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,36 +3,58 @@
 
  // The array of words for the game
 
- 
+
  // Pick a random word
  var game = {
-    wordList: ["bodyguard", "bojack horseman", "when they see us", "archer", "russian doll", "black mirror", "ozark"],
-    word: "",
-    guess: "",
-    currentImg: "",
-    lettersGuessed: "",
-    winCount: 0,
-    guessesLeft: 15,
-    gameStarted: false,
-    instructions: "",
- 
-    
-    // FUNCTIONS
-    // ==============================================================================
+     wordList: ["bodyguard", "bojack horseman", "when they see us", "archer", "russian doll", "black mirror", "ozark"], //list of words
+     word: "", //randomly selected word
+     displayWord: "",
+     wordArr: [],
+     guess: "", //letter guessed by user
+     currentImg: "", //image clue
+     lettersGuessed: [], // letters user has already guessed
+     winCount: 0, //number of wins
+     guessesLeft: 10, //number of guesses remaining
+     validGuesses: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+     instructionElement: document.getElementById("instruction"),
+     wordToGuess: document.getElementById("wordToGuess"),
+     guessesRemainingElement: document.getElementById("guessesRemaining"),
+     lettersGuessedElement: document.getElementById("lettersGuessed"),
+     numberOfWinsElement: document.getElementById("numberofWins"),
 
-startGame: function() {
-    document.getElementById('instruction').textContent = "This picture is a scene from a popular Netflix Show. Select a letter to complete the title!";
-    document.getElementById('instruction').style.fontSize = "x-large";
+     // FUNCTIONS
+     //.=============================================================================
 
+
+
+     //Set up game
+     startGame: function () {
+         this.instructionElement.textContent = "This picture is a scene from a popular Netflix Show. Select a letter to complete the title!";
+         this.instructionElement.style.fontSize = "x-large";
+         //select random word
+         this.word = this.wordList[Math.floor(Math.random() * this.word.length)];
+         console.log(this.word);
+         //display random word
+         for (var i = 0; i < this.word.length; i++) {
+             if (this.word[i] === " ") {
+                 this.displayWord[i] = "_";
+             } else {
+                 this.displayWord[i] = " "
+             };
+             this.wordToGuess.textContent = this.displayWord;
+             console.log(this.displayWord);
+            }
+        }
     }
-};
 
-   
- // MAIN PROCESS
- // ==============================================================================
+//Check guesss
 
- // Press key to get started
 
- document.onkeyup = function(event) {
-     game.startGame();
- }
+     // MAIN PROCESS
+     // ==============================================================================
+
+     // Press key to get started
+
+     document.onkeyup = function (event) {
+         game.startGame();
+     }
