563bf81ec4ab8ce0658c030c06d7880775eae774
Lisa Leu <Lisal@netwealth.com.au>
2019-07-05 15:45:12 +1000

    cleaned up startGame method

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
  // VARIABLES
  // ==========================================================================
 
- // The array of words for the game
+ // HTML elements
  var instructionElement = document.getElementById("instruction");
  var wordToGuess = document.getElementById("wordToGuess");
  var guessesRemainingElement = document.getElementById("guessesRemaining");
@@ -9,11 +9,11 @@
  var numberOfWinsElement = document.getElementById("numberofWins");
 
 
- // Pick a random word
+ // Object variables
  var game = {
          wordList: ["bodyguard", "bojack horseman", "archer", "russian doll", "black mirror", "ozark"], //list of words
          word: "", //randomly selected word
-         wordArr: [],
+         letterArr: [], //letters in the word
          wordObject: {},
          wordStatus: "",
          guess: "", //letter guessed by user
@@ -22,113 +22,44 @@
          winCount: 0, //number of wins
          guessesLeft: 10, //number of guesses remaining
          isInWord: false,
+         blanks: 0, //
+         lettersAndBlanks: [], //
 
-         // METHODS
-         // =============================================================================
-
-
-         //Set up game
-         startGame: function () {
-             instructionElement.textContent = "This picture is a scene from a popular Netflix Show. Select a letter to complete the title!";
-             instructionElement.style.fontSize = "x-large";
-
-             //select random word
-             this.word = this.wordList[Math.floor(Math.random() * this.word.length)];
-             
-             //display random word
-             for (var i = 0; i < this.word.length; i++) {
-                this.wordArr[i] = "_";
-             }
-             wordToGuess.textContent = this.wordArr;  
-             
-             //just checking...
-             console.log(this.word);       
-             console.log(this.wordArr);
-             
-         },
-        
-     
-        //Check guess
-        checkGuess: function (guess) {
-            if (/[A-Z]/.test(guess)) {
-
-                //validate letter against word
-                if (this.wordObject.indexOf(guess) > -1) {
-                    this.correctGuess(guess);
-                } else {
-                    this.wrongGuess(guess);
-                }
-                this.updateText();
-            }
-        },
+// METHODS
+// =============================================================================
 
 
-        correctGuess: function(guess) {
-            // Replace correctly guessed letter in wordStatus (substring method specified the starting position and length for the placement of guess in the the string)
-            for (var i = 0; i < this.wordObject.word.length; i++) {
-                if (this.wordObject.word.charAt(i) == guess) {
-                    this.wordStatus = this.wordStatus.substr(0, i) + guess + this.wordStatus.substr(i + 1);
-                }
-            }
-    
-            // Is the word complete?
-            if (this.wordStatus == this.wordObject.word) {
-                this.winCount++;
-            }
-        },
-    
-        wrongGuess: function(guess) {
-            // Check if letter has already been guessed
-            if (this.lettersGuessed.indexOf(guess) == -1) {
-                var letterSpan;
-                this.lettersGuessed += guess;
+//Set up game
+startGame: function () {
+        instructionElement.textContent = "Select a letter to complete the title of this popular Netflix show!";
+        instructionElement.style.fontSize = "x-large";
 
-    
-                // Add guess to lettersGuessed variable
-                this.lettersGuessed += guess;
-    
-                // If lettersGuessed has 1 or more guesses, add trailing space
-                // Else remove existing space
-                if (this.lettersGuessed.length > 1){
-
-                    getElementById("lettersGuessed").innerHTML = "";
-                }
-    
-                // Add guess to lettersGuessed div
-           
-                lettersGuessedElement.appendChild(letterSpan);
-    
-                // Out of guesses?
-                this.guessesLeft--;
-                if (this.guessesLeft < 1) {
-                    //this.updateImage();
-                    //this.playSound("lose");
-                    this.newWord();
-               // } else {
-                 //   this.playSound("wrong");
-                }
-            }
-        },
-
-            UpdateText: function() {
-                // Update all text fields on the UI
-                this.winCount.textContent = this.winCount;
-                this.wordStatus.textContent = this.wordStatus;
-                this.guessesRemaining.textContent = this.guessesLeft;
-            }
+        //select random word
+        this.word = this.wordList[Math.floor(Math.random() * this.word.length)];
+        //split letters of the random word
+        this.letterArr = this.word.split('');
+        //counts how many letters are left to be guessed
+        this.blanks = this.letterArr.length;
+        //display random word
+        for (var i = 0; i < this.blanks; i++) {
+            this.letterArr[i] = "_";
         }
+            //send word to guess to UI
+            wordToGuess.textContent = this.lettersAndBlanks;
+
+            //logging (sanity check)
+            console.log("word to guess: " + this.word);
+            console.log(this.letterArr);
+            console.log(this.blanks);
+            console.log(this.lettersAndBlanks);
+    }
+}
 
 
-     // MAIN PROCESS
-     // ==============================================================================
+// MAIN PROCESS
+// ==============================================================================
 
-     // Press key to get started
-
-     document.onkeyup = function (event) {
-         game.startGame();
-     }
-
-     document.onkeyup = function (event) {
-         var guess = String.fromCharCode(event.keyCode).toUpperCase();
-         game.checkGuess(guess);
-     }
+// Press key to get started
+document.onkeyup = function (event) {
+    game.startGame();
+}
